7cc472a0e263dadb7188d08f3e086b47d23a4109
shyndman <scotty.hyndman@gmail.com>
2012-01-26 19:47:18 -0500

    Now uses HTTPS, and includes better error handling

--- pvt.rb
+++ pvt.rb
@@ -7,10 +7,10 @@ require "cgi"
 require "nokogiri"
 
 CONFIG_PATH = File.expand_path "~/.pvt"
-FILTER_API_URL = "http://www.pivotaltracker.com/services/v3/projects/%s/stories?filter=%s"
+FILTER_API_URL = "https://www.pivotaltracker.com:443/services/v3/projects/%s/stories?filter=%s"
 FILTER_FORMAT = "mywork:%s state:started"
 
-# Quick check
+# Check for config file
 if not File.exists? CONFIG_PATH
   $stderr.write "Could not find configuration at #{CONFIG_PATH}\n"
   $stderr.write "See .pvt.sample for information on configuration\n"
@@ -34,12 +34,28 @@ open CONFIG_PATH do |config|
 end
 
 # Construct the URL
-filter = CGI::escape(FILTER_FORMAT % opts[:initials])
+filter = CGI.escape(FILTER_FORMAT % opts[:initials])
 filter_uri = URI.parse(FILTER_API_URL % [opts[:project_id], filter])
 
+# Construct the client
+client = Net::HTTP.new(filter_uri.host, filter_uri.port)
+client.use_ssl = true
+
 # Grab the stories (XML)
-response = Net::HTTP.start(filter_uri.host, filter_uri.port) do |http|
-  http.get("#{filter_uri.path}?#{filter_uri.query}", "X-TrackerToken" => opts[:token])
+response = nil
+begin
+  response = client.get("#{filter_uri.path}?#{filter_uri.query}", "X-TrackerToken" => opts[:token])
+rescue Exception => e
+  $stderr.write "Error while attempting to communicate with Pivotal\n"
+  $stderr.write "Details: #{e}\n"
+  exit 1
+end
+
+# Error handling
+unless response.code.start_with? '2'
+  $stderr.write "Error response received from Pivotal\n"
+  $stderr.write "#{response.code}: #{response.message}\n"
+  exit 1
 end
 
 # Parse out the stories and write them to stdout
